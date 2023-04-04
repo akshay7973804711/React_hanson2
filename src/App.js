73c15206e06bd819fe7896129e0117c33681fb5a
akshay793804711 <akshaypratapsingh4545@gmail.com>
2023-04-04 12:04:42 +0530

    2nd comit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import './App.css';
+import React from 'react';
+import ClassCompo from './Component.js/ClassCompo';
+import './Component.js/Components.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='heading'>EMPLOYEE FEEDBACK FORM</h1><br/>
+    <div id="main">
+    <ClassCompo/>
+
+    </div>
+    </>
+  );
+}
+export default App;
